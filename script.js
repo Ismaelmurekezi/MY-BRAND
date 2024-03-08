9901cdd1e7c9ff33045f087216910382d37689a4
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-03-08 12:52:46 +0200

    adding modifications

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu');
 const navLinks = document.getElementById('navLinks');
+let likeIcon=document.getElementById('likeIcon');
 
 hamburgerMenu.addEventListener('click', () => {
     if (navLinks.classList.contains('show')) {
@@ -11,4 +12,10 @@ hamburgerMenu.addEventListener('click', () => {
     }
 
 });
+
+likeIcon.addEventListener('click',()=>{
+    likeIcon.style.color="#36C0C9";
+    // alert("Thanks for your like")
+})
+
 
