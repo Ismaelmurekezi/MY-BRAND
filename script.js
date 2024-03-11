5ecad21af6e8de2f1b762b933b74f1559d067494
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-03-11 17:32:29 +0200

    adding dashboard

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu');
 const navLinks = document.getElementById('navLinks');
-let likeIcon=document.getElementById('likeIcon');
+let likeIcon=document.querySelectorAll('.likeIcon');
 
 hamburgerMenu.addEventListener('click', () => {
     if (navLinks.classList.contains('show')) {
@@ -13,9 +13,15 @@ hamburgerMenu.addEventListener('click', () => {
 
 });
 
-likeIcon.addEventListener('click',()=>{
-    likeIcon.style.color="#36C0C9";
-    // alert("Thanks for your like")
+//Like icon functionality
+ 
+likeIcon.addEventListener('click',function(){
+    alert("hello")
+  
+
 })
+
+
+
 
 
